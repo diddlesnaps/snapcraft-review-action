1b77a063a85ab17b520fd5757b08e41daf6586b2
Daniel Llewellyn <diddledan@ubuntu.com>
2020-02-05 21:08:28 +0000

    Update src/review.ts
    Add vim header to configure indent size and language
    
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- src/review.ts
+++ src/review.ts
@@ -1,3 +1,5 @@
+// -*- mode: javascript; js-indent-level: 2 -*-
+
 import * as exec from '@actions/exec'
 import * as tools from './tools'
 
@@ -6,59 +8,59 @@ import * as tools from './tools'
 import fs = require('fs') // eslint-disable-line @typescript-eslint/no-require-imports
 
 export class SnapReviewer {
-    snapFile: string
-    plugsDeclFile: string
-    slotsDeclFile: string
-    allowClassic: boolean
-
-    constructor(snapFile: string, plugsDeclFile: string, slotsDeclFile: string, allowClassic: boolean) {
-        this.snapFile = snapFile
-        this.plugsDeclFile = plugsDeclFile
-        this.slotsDeclFile = slotsDeclFile
-        this.allowClassic = allowClassic
+  snapFile: string
+  plugsDeclFile: string
+  slotsDeclFile: string
+  allowClassic: boolean
+  
+  constructor(snapFile: string, plugsDeclFile: string, slotsDeclFile: string, allowClassic: boolean) {
+    this.snapFile = snapFile
+    this.plugsDeclFile = plugsDeclFile
+    this.slotsDeclFile = slotsDeclFile
+    this.allowClassic = allowClassic
+  }
+  
+  async validate(): Promise<void> {
+    try {
+      await fs.promises.access(this.snapFile, fs.constants.R_OK)
+    } catch (error) {
+      throw new Error(`cannot read snap file "${this.snapFile}"`)
     }
-
-    async validate(): Promise<void> {
-        try {
-            await fs.promises.access(this.snapFile, fs.constants.R_OK)
-        } catch (error) {
-            throw new Error(`cannot read snap file "${this.snapFile}"`)
-        }
-        try {
-            if (this.plugsDeclFile) {
-                await fs.promises.access(this.plugsDeclFile, fs.constants.R_OK)
-            }
-        } catch {
-            throw new Error(`cannot read plugs declaration file "${this.plugsDeclFile}"`)
-        }
-        try {
-            if (this.slotsDeclFile) {
-                await fs.promises.access(this.slotsDeclFile, fs.constants.R_OK)
-            }
-        } catch {
-            throw new Error(`cannot read slots declaration file "${this.slotsDeclFile}"`)
-        }
+    try {
+      if (this.plugsDeclFile) {
+        await fs.promises.access(this.plugsDeclFile, fs.constants.R_OK)
+      }
+    } catch {
+      throw new Error(`cannot read plugs declaration file "${this.plugsDeclFile}"`)
     }
-
-    async runReviewTools(): Promise<void> {
-        const args: string[] = []
-        if (this.plugsDeclFile) {
-            args.push('--plugs', this.plugsDeclFile)
-        }
-        if (this.slotsDeclFile) {
-            args.push('--slots', this.slotsDeclFile)
-        }
-        if (this.allowClassic) {
-            args.push('--allow-classic')
-        }
-        args.push(this.snapFile)
-        await exec.exec('review-tools.snap-review', args)
-
+    try {
+      if (this.slotsDeclFile) {
+        await fs.promises.access(this.slotsDeclFile, fs.constants.R_OK)
+      }
+    } catch {
+      throw new Error(`cannot read slots declaration file "${this.slotsDeclFile}"`)
     }
-
-    async review(): Promise<void> {
-        await tools.ensureSnapd()
-        await tools.ensureReviewTools()
-        await this.runReviewTools()
+  }
+  
+  async runReviewTools(): Promise<void> {
+    const args: string[] = []
+    if (this.plugsDeclFile) {
+      args.push('--plugs', this.plugsDeclFile)
     }
+    if (this.slotsDeclFile) {
+      args.push('--slots', this.slotsDeclFile)
+    }
+    if (this.allowClassic) {
+      args.push('--allow-classic')
+    }
+    args.push(this.snapFile)
+    await exec.exec('review-tools.snap-review', args)
+    
+  }
+  
+  async review(): Promise<void> {
+    await tools.ensureSnapd()
+    await tools.ensureReviewTools()
+    await this.runReviewTools()
+  }
 }
